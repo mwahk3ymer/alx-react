13f3bdedf3ff65aa8157835ee782b704ed8dd67e
mwahk3ymer <audreymwakima99@gmail.com>
2024-04-03 07:40:45 +0000

    task 0

--- 0x00-Webpack/task_0/0x00-Webpack/task_0/index.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-
-$(document).ready(function() {
-  // Create and append elements using jQuery
-  $('<p>').text('Paragraph 1').appendTo('body');
-  $('<p>').text('Paragraph 2').appendTo('body');
-  $('<p>').text('Paragraph 3').appendTo('body');
-});
