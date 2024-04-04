c4846f2b5f18b4580ac0a14780fe0e41041b7ed8
mwahk3ymer <audreymwakimaa99@gmail.com>
2024-04-04 11:10:29 +0000

    files removed

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard<h1>
+        <h1>School dashboard</h1>
 	  </header>
 	  <div className="App-body">
 	  <p>Login to access the full dashboard</p>
