4f8be042acb4d8a31246b03ae5619047d73810a8
mwahk3ymer <audreymwakima99@gmail.com>
2024-04-24 11:51:27 +0000

    react 0

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,12 @@
-const Immutable = require('immutable');
+// Import the fromJS function from Immutable.js library
+const { fromJS } = require('immutable');
 
+// Define the function getImmutableObject
 function getImmutableObject(object) {
-    return Immutable.fromJS(object);
+    // Convert the object into an immutable Map using fromJS
+    const immutableMap = fromJS(object);
+    return immutableMap;
 }
+
+// Export the getImmutableObject function
+module.exports = getImmutableObject;
