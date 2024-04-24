2ebb58be0d54b8bd0509e395186daa8988cc221f
mwahk3ymer <audreymwakima99@gmail.com>
2024-04-24 11:30:12 +0000

    correction 0

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -3,14 +3,3 @@ const Immutable = require('immutable');
 function getImmutableObject(object) {
     return Immutable.fromJS(object);
 }
-
-// Example usage
-const obj = {
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
-};
-
-const immutableMap = getImmutableObject(obj);
-console.log(immutableMap);
