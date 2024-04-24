a224353085ef4aaba97c118c195270c38d1a9015
mwahk3ymer <audreymwakima99@gmail.com>
2024-04-24 13:12:36 +0000

    accesing nested elements1

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -4,21 +4,12 @@ export default function accessImmutableObject(object, array) {
 
     // Iterate through each key in the array
     for (const key of array) {
-        // Check if the current value is a Map
-        if (value instanceof Map) {
-            // Attempt to get the value at the current key
-            value = value.get(key);
-        } else if (typeof value === 'object' && value !== null) {
-            // If the current value is an object (plain JavaScript object)
+        // Check if the current value is defined and is an object
+        if (value && typeof value === 'object' && !Array.isArray(value)) {
             // Attempt to access the value at the current key
             value = value[key];
         } else {
-            // If the current value is neither a Map nor an object, return undefined
-            return undefined;
-        }
-
-        // If the value is undefined, return undefined
-        if (value === undefined) {
+            // If the current value is not an object or the key doesn't exist, return undefined
             return undefined;
         }
     }
