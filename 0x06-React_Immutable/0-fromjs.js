c039cd5f83544a8b1089731c7f1fc178012a2667
mwahk3ymer <audreymwakima99@gmail.com>
2024-04-24 12:53:47 +0000

    rerun

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,30 +1,5 @@
-// Importing the Immutable.js library
-const { Map, fromJS } = require('immutable');
+import {fromJS } from './node_modules/immutable/dist/immutable';
 
-/**
- * Converts a JavaScript object into an Immutable Map using fromJS.
- * @param {Object} object - The JavaScript object to convert.
- * @returns {Map} - The Immutable Map representation of the object.
- */
-function getImmutableObjectFromJS(object) {
-    // Using fromJS to convert the object to an Immutable Map
-    const immutableMap = fromJS(object);
-    return immutableMap;
-}
+const getImmutableObject = (object) => fromJS(object);
 
-/**
- * Converts a JavaScript object into an Immutable Map using Map constructor.
- * @param {Object} object - The JavaScript object to convert.
- * @returns {Map} - The Immutable Map representation of the object.
- */
-function getImmutableObjectUsingMap(object) {
-    // Using the Map constructor to create an Immutable Map from the object
-    const immutableMap = Map(object);
-    return immutableMap;
-}
-
-// Exporting the functions to make them accessible from other files
-module.exports = {
-    getImmutableObjectFromJS,
-    getImmutableObjectUsingMap
-};
+export default getImmutableObject;
