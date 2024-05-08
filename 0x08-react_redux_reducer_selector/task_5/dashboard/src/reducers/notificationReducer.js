f2348d0228fe1e4cbd65006b7bf5964dab7b550b
mwahk3ymer <audreymwakima99@gmail.com>
2024-05-08 13:24:51 +0000

    task5

---
+++ 0x08-react_redux_reducer_selector/task_5/dashboard/src/reducers/notificationReducer.js
@@ -0,0 +1,26 @@
+// notificationReducer.js
+import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_TYPE_FILTER } from './notificationActionTypes';
+import { fromJS } from 'immutable';
+import { notificationsNormalizer } from '../schema/notifications';
+
+const initialState = fromJS({
+  filter: 'DEFAULT',
+  notifications: {}
+});
+
+const notificationReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_NOTIFICATIONS_SUCCESS: {
+      const normalizedData = notificationsNormalizer(action.data);
+      return state.mergeIn(['notifications'], normalizedData.entities.notifications);
+    }
+    case MARK_AS_READ:
+      return state.setIn(['notifications', action.id, 'isRead'], true);
+    case SET_TYPE_FILTER:
+      return state.set('filter', action.filter);
+    default:
+      return state;
+  }
+};
+
+export default notificationReducer;
