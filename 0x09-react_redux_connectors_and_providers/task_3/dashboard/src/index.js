efedb1d649d02e17fcb1a797fdd8ee506e07a71a
mwahk3ymer <audreymwakimaa99@gmail.com>
2024-05-14 12:10:44 +0000

    redux extension1

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,11 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers'; // assuming rootReducer is your combined reducers
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import rootReducer from './reducers'; // Import your root reducer here
+import App from './App'; // Import your root component here
 
+// Create the Redux store with middleware and Redux DevTools
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
 
-export default store;
+// Render the root component wrapped in the Redux Provider
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
